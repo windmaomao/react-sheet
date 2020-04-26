a9817b2829149c078854f6cb6d38e2798b41f61c
Fang <windmaomao@gmail.com>
2020-04-26 10:01:49 -0400

    add plus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Plus } from './components'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       Hello Sheet
+      <Plus />
     </div>
   )
 }
