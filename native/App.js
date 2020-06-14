658bb0e863ecf0e33a7c7ebc23c243994ccf1cd7
Fang <windmaomao@gmail.com>
2020-06-14 17:07:34 -0400

    attach sheet to app

--- native/App.js
+++ native/App.js
@@ -1,13 +1,19 @@
 import 'react-native-gesture-handler'
-import * as React from 'react'
+import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Splash from './pages/Splash'
 import Home from './pages/Home'
+import questionGen from './utils/questionGen'
+import sheetGen from './utils/sheetGen'
+
+const gen = questionGen()
+const defaultSheet = sheetGen(gen).createSheet(100)
 
 const Stack = createStackNavigator()
 
 export default () => {
+  const [sheet, setSheet] = useState(defaultSheet)
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -16,11 +22,9 @@ export default () => {
           component={Splash}
           options={{ title: 'Welcome' }}
         />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Home' }}
-        />
+        <Stack.Screen name="Home">
+          {props => <Home {...props} sheet={sheet} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
