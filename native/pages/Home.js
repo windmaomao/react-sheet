a51213798cfcf16d273c2ed549ac667840f56848
Fang <windmaomao@gmail.com>
2020-06-14 17:09:30 -0400

    relocate questions to app

--- native/pages/Home.js
+++ native/pages/Home.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import { FlatList } from 'react-native'
 import Question from './Question'
-import arithmeticGen from '../utils/arithmeticGen'
-import sheetCreator from '../utils/sheetCreator'
 
-const gen = arithmeticGen(10, '+')
-const sheet = sheetCreator(gen).createSheet(100)
-
-const questions = [
-  { i: 1, first: 2, second: 8, op: '+' },
-  { i: 2, first: 6, second: 18, op: '+' },
-  { i: 3, first: 8, second: 2, op: '+' },
-  { i: 4, first: 9, second: 3, op: '+' },
-]
-
-export default function Home() {
+const Home = ({ sheet }) => {
   return (
     <FlatList
       data={sheet.questions}
@@ -23,3 +11,5 @@ export default function Home() {
     />
   )
 }
+
+export default Home
