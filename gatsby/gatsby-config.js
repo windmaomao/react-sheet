9c06cead2a34cb25f98aa0c59ba35d2bddd14473
Fang <windmaomao@gmail.com>
2020-06-18 13:26:03 -0400

    add my files

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -8,10 +8,19 @@ module.exports = {
   siteMetadata: {
     title: 'Qplot Gatsby'
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-typography',
-    options: {
-      pathToConfigModule: `src/utils/typography`,      
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },    
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: `src/utils/typography`,      
+      }
     }
-  }],
+  ],
 }
