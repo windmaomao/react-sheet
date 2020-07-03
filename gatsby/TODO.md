32766840820f1cd51bd02c3519663a6376e7e1d6
Fang <windmaomao@gmail.com>
2020-07-03 17:30:26 -0400

    add favicon

--- gatsby/TODO.md
+++ gatsby/TODO.md
@@ -1,5 +1,13 @@
 # Todos
 
+## 07/03
+
+- add tags to posts
+- support rss
+- support dark theme
+- add site icon
+- ~~add plus component~~
+
 ## 06/20
 
 - ~~add tags~~
--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -23,6 +23,12 @@ module.exports = {
     ],
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/logo.png',
+      },
+    },
     `gatsby-theme-blog`,
     `gatsby-theme-blog-tags`
   ],
