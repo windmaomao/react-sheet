4d1d98d2dd1313fd1e0ff93113d2ba694b99c33c
Fang <windmaomao@gmail.com>
2020-07-16 17:21:49 -0400

    add loader

--- gatsby/src/components/Github/Repos.js
+++ gatsby/src/components/Github/Repos.js
@@ -3,17 +3,25 @@ import { fbAuth } from '../../utils'
 
 export default () => {
   const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     async function fetch() {
-      const repos = await fbAuth.getRepos()
-      setRepos(repos.reverse())
+      try {
+        setLoading(true)
+        const repos = await fbAuth.getRepos()
+        setRepos(repos.reverse())
+      }
+      finally {
+        setLoading(false)
+      }
     }
     fetch()
   }, [setRepos])
 
   return (
     <ul>
+      {loading && 'loading ...'}
       {repos.map(repo => (
         <li key={repo.id} title={repo.created_at}>
           {repo.name} ({repo.size})
