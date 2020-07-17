c0989107f7d9be737d25359264ed4d46bcbb2418
Fang <windmaomao@gmail.com>
2020-07-17 17:55:26 -0400

    update profile

--- gatsby/src/components/Github/Profile.js
+++ gatsby/src/components/Github/Profile.js
@@ -1,28 +1,38 @@
 import React from "react"
-import { Avatar } from "theme-ui"
+import { Avatar, IconButton, Button } from "theme-ui"
+import { AiOutlineLogout, AiOutlineGithub } from "react-icons/ai";
 
 export default ({
   user, onLogin, onLogout
 }) => {
   if (!user) {
     return (
-      <button
-        type="button"
+      <Button
         onClick={onLogin}
-      >Sign In Github</button>
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          cursor: 'pointer'
+        }}
+      >
+        <AiOutlineGithub size="1.5rem" />
+        &nbsp;Sign in Github
+      </Button>
     )
   }
 
   return (
-    <div>
+    <>
       <Avatar
         title={user.displayName}
         src={user.photoURL}
       />
-      <button
-        type="button"
+      <IconButton
         onClick={onLogout}
-      >Logout</button>
-    </div>
+        size="sm"
+      >
+        <AiOutlineLogout size="2em" />
+      </IconButton>
+    </>
   )
 }
