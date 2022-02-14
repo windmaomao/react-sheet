cee61c5891b44abfd9329b4d83649f2bddcf8999
Fang Jin <windmaomao@gmail.com>
2022-02-14 15:23:07 -0500

    draft basic theming toggle button

--- next/pages/dict.tsx
+++ next/pages/dict.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import ThemeToggle from 'components/ThemeToggle'
 import DictSheet from 'components/DictSheet'
 
 const DictPage: NextPage = () => {
   return (
     <div>
       <Head> <title>Learn A Word</title> </Head>
+      <ThemeToggle />
       <DictSheet />
     </div>
   )
