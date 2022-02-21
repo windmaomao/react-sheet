29abbc852e5d0c8ea6339c9f848f4ed87246b274
Fang Jin <windmaomao@gmail.com>
2022-02-21 15:15:08 -0500

    make the dict page working with hookstate

--- next/components/DictSettings.tsx
+++ next/components/DictSettings.tsx
@@ -2,7 +2,7 @@ import {motion} from 'framer-motion'
 import Modal from 'components/Modal'
 import {useState} from 'react'
 import styles from './DictSettings.module.css'
-import {useDictSettings, setDictGen} from 'components/useDictSettings'
+import {useDictSettings} from 'components/useDictSettings'
 import dictGen from 'services/dictGen'
 
 const DictSettings = ({}) => {
@@ -14,7 +14,6 @@ const DictSettings = ({}) => {
   const [day, setDay] = useState('1')
   const onDay = (d: string) => () => {
     setDay(d)
-    // setDictGen(d)
     dict.day.set(d)
     dict.gen.set(dictGen(d))
   }
--- next/components/useDictSettings.tsx
+++ next/components/useDictSettings.tsx
@@ -1,8 +1,10 @@
 import {createState, useState} from '@hookstate/core'
+import dictGen from 'services/dictGen'
 
 const initDictSettings = createState({
   days: ['custom', 'day1', 'day2'],
-  day: 'day1'
+  day: 'day1',
+  gen: dictGen('day1') 
 })
 
 export const useDictSettings = () => {
