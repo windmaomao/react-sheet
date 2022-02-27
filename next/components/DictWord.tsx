922310f14f047b9eae4584256d337867d8a43076
Fang Jin <windmaomao@gmail.com>
2022-02-27 10:31:02 -0500

    add speech to speak dict word

--- next/components/DictWord.tsx
+++ next/components/DictWord.tsx
@@ -1,16 +1,30 @@
 import styles from './DictWord.module.css'
 import {QuestionProps} from 'components/PageSheet'
 import {useSelector} from 'states/useStore'
+import {useEffect} from 'react'
 
-const DictWord = ({ id, touch }: QuestionProps) => {
+const DictWord = ({ id, touch, active }: QuestionProps) => {
   const w = useSelector(s => s.dict.words[id as number])
   if (!w) return null
+
+  const onClick = () => {
+    const u = new SpeechSynthesisUtterance()
+    u.lang = 'en'
+    u.pitch = 1.0
+    u.rate = 0.75
+    u.text = w ? w.word : 'Hello' 
+    window.speechSynthesis.speak(u)
+  }
+
+  useEffect(() => {
+    if (active) { onClick() } 
+  }, [active])
 
   return (
     <div className={styles.word}
          onClick={touch}
     >
-      <div title={w.meaning}>{w.word}</div>
+      <div title={w.meaning} onClick={onClick}>{w.word}</div>
     </div> 
   )
 }
